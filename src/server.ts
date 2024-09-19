8ee1a53a7264b9f1fbb83c3ecfc914cfdd2ebae8
iuritorres <iuri.t1000@gmail.com>
2024-09-19 11:44:00 -0300

    docs: add swagger api docs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "../docs/swagger.json";
 import { env } from "./env.ts";
 import { errorMiddleware } from "./middlewares/error.ts";
 import { apiRouter } from "./routes/index.ts";
@@ -8,6 +10,9 @@ import { apiRouter } from "./routes/index.ts";
 export const server = express();
 server.use(express.json());
 server.use(cors());
+
+server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 server.use("/api", apiRouter);
 server.use(errorMiddleware);
 
